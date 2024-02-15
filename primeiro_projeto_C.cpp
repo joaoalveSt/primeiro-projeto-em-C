f4733516ea1ccf37e3b9bcbb0ebaf468fe8017af
joão frutuoso <97484261+joaoalveSt@users.noreply.github.com>
2024-02-15 15:05:01 -0300

    Add files via upload

---
+++ primeiro_projeto_C.cpp
@@ -0,0 +1,13 @@
+
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int idade = 15; //isso e uma variavel
+    
+    printf("Hello World ! \nMeu Nome e João\nele tem %d anos\nsim ele tem %d anos", idade, idade);
+    return 0;//o %d significa a vairiavel
+}
